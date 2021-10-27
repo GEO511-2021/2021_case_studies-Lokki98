bd9ef4ccbd05fc5dfc0cbdd41e6f23c122917a5f
ZiqiTang98 <89820898+Lokki98@users.noreply.github.com>
2021-10-27 16:18:06 -0400

    name of path

--- week_03/case_study_03.R
+++ week_03/case_study_03.R
@@ -16,7 +16,7 @@ gapminder_continent <- gapminder %>%
             gdpPercapweighted = weighted.mean(x = gdpPercap, w = pop),
             pop = sum(as.numeric(pop)))
 gapminder$country
-ggsave("D://R/tmp//2021_case_studies-Lokki98//week_03//wealth&life1.png",device = "png",width = 15)
+ggsave("wealth&life1.png",device = "png",width = 15)
 
 ggplot(gapminder,aes(group = country)) +
   geom_point(aes(x = year,y = gdpPercap,color = continent,size=pop/1000000)) +
@@ -30,7 +30,7 @@ ggplot(gapminder,aes(group = country)) +
   ) + 
   labs(title = "Wealth and life expectancy through time")
 
-ggsave("D://R/tmp//2021_case_studies-Lokki98//week_03//wealth&life2.png",device = "png",width = 15)
+ggsave("wealth&life2.png",device = "png",width = 15)
 
 
 
